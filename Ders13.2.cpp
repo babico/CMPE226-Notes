56ed5a352acac4cb6a057081bfaf9b6be1ff03c3
Müslüm Barış Korkmazer <boran_nesil@hotmail.com>
2022-04-03 19:35:44 +0300

    03.04.2022

---
+++ Ders13.2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stack>
+#include <list>
+
+// STL CLASS STACK
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    stack<int, list<int>> st;
+    st.push(5);
+    auto x = st.top();
+    cout << x;
+    return 0;
+}
