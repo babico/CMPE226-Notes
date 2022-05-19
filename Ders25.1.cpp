e8c01fa5c896075bf27e56e25106d334641c8c13
babico <boran_nesil@hotmail.com>
2022-05-19 05:03:09 +0300

    19.05.2022

---
+++ Ders25.1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+// HEAP SORT
+// Binary trees as lists/array
+
+/*
+    The left child of node k is in position 2k+1
+    The right child of node k is in position 2k+2
+*/
+
+// HEAP :
+// A LIST IN WHOCH FOR ALL POSITION k IN THE LIST
+// THE KEY AT POSITION k IS AT LEAST AS LARGE AS
+// THE KEYS IN POSITIONS 2k+1 & 2K+2 İF THEY EXIST.
+// [CONTENT OF EACH NODE <= FATHER]
+
+using namespace std;
+
+int main()
+{
+
+}
